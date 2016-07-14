965d60146a368a1cc43774c61206f4f80c3745ca
Yang Eric <ygweric@163.com>
2016-07-14 15:18:57 +1200

    set `accessibilityLabel` so I can get the special UIAlertAction when I use EarlGrey for UI Test

--- EZAlertController.swift
+++ EZAlertController.swift
@@ -56,7 +56,7 @@ public class EZAlertController {
     
     public class func alert(title: String, message: String, acceptMessage: String, acceptBlock: () -> ()) -> UIAlertController {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
-        let acceptButton = UIAlertAction(title: acceptMessage, style: .Default, handler: { (action: UIAlertAction) in
+        let acceptButton = UIAlertAction.action(title: acceptMessage, style: .Default, handler: { (action: UIAlertAction) in
             acceptBlock()
         })
         alert.addAction(acceptButton)
@@ -85,7 +85,7 @@ public class EZAlertController {
     public class func actionSheet(title: String?, message: String, sender: AnyObject, buttons:[String], cancel: String? = nil, tapBlock:((UIAlertAction,Int) -> Void)?) -> UIAlertController{
         let alert = UIAlertController.alert(title: title, message: message, preferredStyle: .ActionSheet, buttons: buttons, tapBlock: tapBlock)
         if cancel != nil {
-            let cancelActionButton: UIAlertAction = UIAlertAction(title: cancel, style: .Cancel) { action -> Void in
+            let cancelActionButton: UIAlertAction = UIAlertAction.action(title: cancel, style: .Cancel) { action -> Void in
                 
                 if let block = tapBlock {
                     block(action,-1)
@@ -112,7 +112,7 @@ private extension UIAlertController {
         let alert = UIAlertController(title: title, message: message, preferredStyle:preferredStyle)
         var buttonIndex = 0
         for buttonTitle in buttons {
-            let action = UIAlertAction(title: buttonTitle, preferredStyle: .Default, buttonIndex: buttonIndex, tapBlock: tapBlock)
+            let action = UIAlertAction.action(title: buttonTitle, preferredStyle: .Default, buttonIndex: buttonIndex, tapBlock: tapBlock)
             buttonIndex += 1
             alert.addAction(action)
         }
@@ -120,15 +120,25 @@ private extension UIAlertController {
     }
 }
 
-
+let actionAccessibility = "accessibility_label_%@"
 
 private extension UIAlertAction {
-    convenience init(title: String?, preferredStyle: UIAlertActionStyle, buttonIndex:Int, tapBlock:((UIAlertAction,Int) -> Void)?) {
-        self.init(title: title, style: style) {
+    
+    class func action(title title: String?, style: UIAlertActionStyle, handler: ((UIAlertAction) -> Void)?) -> UIAlertAction {
+        let action = UIAlertAction(title: title, style: style, handler: handler)
+        action.accessibilityLabel = String(format: actionAccessibility, title!)
+        
+        return action
+    }
+    
+    class func action(title title: String?, preferredStyle: UIAlertActionStyle, buttonIndex:Int, tapBlock:((UIAlertAction,Int) -> Void)?) -> UIAlertAction {
+        let action = UIAlertAction.action(title: title, style: preferredStyle) {
             (action:UIAlertAction) in
             if let block = tapBlock {
                 block(action,buttonIndex)
             }
         }
+        
+        return action
     }
 }
